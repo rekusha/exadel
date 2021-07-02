d1a536f89a7efcdc646bac71cbce82763716442a
Alexandr Rekun <rekusha@gmail.com>
2021-07-02 19:04:59 +0300

    Create postgres.sql

---
+++ task6/postgres.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE task6;
+
+\c task6
+
+create table if not exists Students (
+				id serial primary key,
+                Student varchar (127) not null,
+                StudentId int not null);
+
+create table if not exists Result (
+				id serial primary key,
+                StudentId int not null,
+                Task1 varchar (127) not null,
+                Task2 varchar (127) not null,
+                Task3 varchar (127) not null,
+                Task4 varchar (127) not null);
