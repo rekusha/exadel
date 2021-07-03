f1f13ccde1cec57050a85496e70387e2dd033afe
Alexandr Rekun <rekusha@gmail.com>
2021-07-03 17:37:48 +0300

    Update postgres_init.sql

--- task6/postgres_init.sql
+++ task6/postgres_init.sql
@@ -2,15 +2,18 @@ CREATE DATABASE task6;
 
 \c task6
 
-create table if not exists Students (
-				id serial primary key,
+create table if not exists Students(
+		id serial primary key,
                 Student varchar (127) not null,
                 StudentId int not null);
 
-create table if not exists Result (
-				id serial primary key,
+create table if not exists Result(
+		id serial primary key,
                 StudentId int not null,
                 Task1 varchar (127) not null,
                 Task2 varchar (127) not null,
                 Task3 varchar (127) not null,
                 Task4 varchar (127) not null);
+
+\copy Students(StudentId,Surname Name) FROM './students.csv' delitimer ',' csv header;
+\copy Result(StudentId,Task1,Task2,Task3,Task4) FROM './result.csv' delitimer ',' csv header;
